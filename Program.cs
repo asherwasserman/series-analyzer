eb8516bbcd17c907110a278c5cee96e56ac54242
asher <aw0534145493@gmail.com>
2025-05-12 17:38:13 +0300

    adding a sort functin and areverse function

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace SeriesAnlyzer
+﻿using System.Collections.Immutable;
+
+namespace SeriesAnlyzer
 {
  class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] seriesArr)
         {
+            List<int> series = new List<int>();
+            for (int i = 0; i < seriesArr.Length; i++)
+            {
+                int num = Convert.ToInt32(seriesArr[i]);
+                series.Add(num);
+            }
 
             //Main function that displays a menu
             void menu()
@@ -17,7 +25,8 @@
             {
                 List<int> series = new List<int>();
                 bool doasANumber = false;
-                while (doasANumber == false) 
+                while (doasANumber == false && series.Count < 3) 
+
                 {
                     Console.WriteLine("please enter a series: ");
                     string input = Console.ReadLine()!;
@@ -41,13 +50,23 @@
             //receiving a series - Returens the series in sorted order.
             List<int> sort()
             {
-
+                List<int> newSeries = new List<int>();
+                for(int i = 0;i < series.Count; i++)
+                {
+                    newSeries.Add(series[i]);
+                }
+                return newSeries;
             }
 
             //receiving a series - Returns the series in reverse
-            List<int> reverse()
+            List<int> reverse(List <int> series)
             {
-
+                List<int> newSeries = new List<int>();
+                for(int i = series.Count; i >=0; i--)
+                {
+                    newSeries.Add(series[i]);
+                }  
+                return newSeries;
             }
 
             //receiving a series - Returns the maximum term in the series.
