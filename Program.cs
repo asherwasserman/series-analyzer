d0283aa1e077b8a834590f55dd45244763e42439
asher <aw0534145493@gmail.com>
2025-05-12 16:34:33 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SeriesAnlyzer
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Main function that displays a menu
+            void menu()
+            {
+
+            }
+
+            //Allows you to change the series
+            List<int> changingTheSeries()
+            {
+                return 0;
+
+            }
+
+            //Returens the series in sorted order.
+            List<int> sorted()
+            {
+
+            }
+
+            //Returns the series in reverse
+            List<int> reverse()
+            {
+
+            }
+
+            //Returns the maximum term in the series.
+            int max()
+            {
+
+            }
+
+            //Returns the minimum term in the series.
+            int min()
+            {
+
+            }
+
+            //Returns the sum of the elements in the series
+            int sumOfElements()
+            {
+
+            }
+
+            //Returns the sum of all elements in a series
+            int sumOfAll()
+            {
+
+            }
+
+            //Returns the average of the array elements
+            int average()
+            {
+
+            }
+        }
+    }
+}
